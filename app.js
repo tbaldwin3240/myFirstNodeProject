218780624dddb3041ddf7d5fb5a3b78405371d92
COR Coding <Tbaldwin3240@gmail.com>
2022-05-03 13:42:45 -0400

    Added a new selection of music

--- app.js
+++ app.js
@@ -1,20 +1,23 @@
-modole.exports =
- [{  albumNumber: "1",
-    name:"The Carter",
-    publishDate:"2004",
-    imgURL:"https://upload.wikimedia.org/wikipedia/en/6/6f/LilWayneRebirth.jpg",
-   songTitles:["song1","song2", "song3", "song4"]
+module.exports =
+ [{  albumNumber: "0",
+    name:"God Chaser",
+    publishDate:"2013",
+    imgURL:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRUvDCLv_PjD0Esn-gJh0XlZSJi92-arAaDpA&usqp=CAU",
+   songTitles:["God Chaser","You Reign", "In Your Hands", "It's Working","Already Getting Better","Higher","Under My Feet(The Shout)", "Write A Song","Nothing Can Take My Praise","Praying For You","Make Me Righteous","Make Me Righteous(Reprise)","You Are My Strength"]
+ },
+ {
+    albumNumber:"1",
+    name:"The Collection Daryl Coley",
+    publishDate:"1986",
+    imgURL:"https://m.media-amazon.com/images/I/41QW91855QL.jpg",
+   songTitles:["I Can't Tell It All","God And God Alone", "He's Right on Time", "Real","He's Preparing Me","It Shall Be Done","He That Dwelleth","He Delivered Me", "To Live os Christ", "Sovereign","When the Music Stops","Vitory in Jesus"]
  },
  {
     albumNumber:"2",
-    name:"Free Weezy",
-    description:"2015",
-    imgURL:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTyYQZp-Sd5KJf06zqLMT-9TqJQkBSTlBhJYB-lQbOXJR6ajg7uq2hZdF4uqa5amDr-5OY&usqp=CAU",
-   songTitles:["song1","song2", "song3", "song4"]
+    name:"Silk Sonic",
+    publishDate:"2021",
+    imgURL:"https://media.pitchfork.com/photos/618a3190a06d7bd133209ff8/1:1/w_600/100000x100000-999.jpeg",
+   songTitles:["Leave The Door Open","Fly As Me", "After Last Night(With Thundercat & Bootsy Collins", "Smokin Out The Window","Put On A Smile","777","Skate","Blast Off"]
  }
-];
 
-
-
-
-
+ ]
